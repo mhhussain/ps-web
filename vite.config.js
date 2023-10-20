21fc386b12331d29c3cc02f9f93d6038c4a8814f
mhhussain <2152584+mhhussain@users.noreply.github.com>
2023-10-20 13:43:49 -0400

    changed based

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ps-web/',
   plugins: [
     vue({
       template: { transformAssetUrls }
