0bba67d54bb5cd681079e442e19164f5674aeb3f
mhhussain <2152584+mhhussain@users.noreply.github.com>
2023-10-20 13:13:24 -0400

    initial codebase for ps website

---
+++ pentagon-solutions-web/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <v-img src="@/assets/pentagon-solutions-logo.jpg" />
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
